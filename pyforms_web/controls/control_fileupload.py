901cfe09cf6287dc829794c08c3a9c3f5e0d9282
João Baúto <joao.m.f.bauto@gmail.com>
2019-01-18 15:23:42 +0000

    added options to change the message previewed by the control. filename length can now be set.

--- pyforms_web/controls/control_fileupload.py
+++ pyforms_web/controls/control_fileupload.py
@@ -5,15 +5,35 @@ import simplejson
 
 class ControlFileUpload(ControlBase):
 
+	def __init__(self, *args, **kwargs):
+		super(ControlFileUpload, self).__init__(*args, **kwargs)
+		self._button = None
+		self._feedback = None
+		self._feedback2 = None
+		self._drop = None
+		self._remove_confirmation = None
+		self._limit_name = 15
+
 	def init_form(self):
-		return "new ControlFileUpload('{0}', {1})".format( self._name, simplejson.dumps(self.serialize()) )
+		return "new ControlFileUpload('{0}', {1})".format(self._name,
+														  simplejson.dumps(self.serialize()) )
 
 	@property
 	def filepath(self):
-		return os.path.join( settings.MEDIA_ROOT, self.value[len(settings.MEDIA_URL):] )
+		if self.value:
+			return os.path.join(settings.MEDIA_ROOT, self.value[len(settings.MEDIA_URL):])
+		else:
+			return None
 
 	def serialize(self):
 		data 	  = super(ControlFileUpload, self).serialize()
+		data.update({'button':self._button,
+					 'feedback':self._feedback,
+					 'feedback2':self._feedback2,
+					 'drop':self._drop,
+					 'remove_confirmation':self._remove_confirmation,
+					 'limit_name': self._limit_name,})
+
 		if self.value:
 			try:
 				file_data = {
